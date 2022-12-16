e1364ef59910e2abeca87d6fee41420d77fb2b5c
sohag-84 <injamulhaqsohag19977@gmail.com>
2022-12-17 00:50:10 +0600

    splash screen is completed

--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -1,4 +1,10 @@
+// ignore_for_file: prefer_const_constructors, unused_local_variable
+
+import 'dart:async';
+
+import 'package:covid_19_tracker/views/world_states_screen.dart';
 import 'package:flutter/material.dart';
+import 'dart:math' as math;
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -7,15 +13,66 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  late final AnimationController _controller = AnimationController(
+    duration: const Duration(seconds: 3),
+    vsync: this,
+  )..repeat();
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      Duration(seconds: 3),
+      () => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => WorldStateScreen(),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+
     return Scaffold(
       body: SafeArea(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
-          children: [],
+          children: [
+            AnimatedBuilder(
+              animation: _controller,
+              builder: (context, child) {
+                return Transform.rotate(
+                  angle: _controller.value * 2.0 * math.pi,
+                  child: child,
+                );
+              },
+              child: Center(
+                child: SizedBox(
+                  height: height * 0.6,
+                  width: width * 0.6,
+                  child: Image(image: AssetImage('images/virus.png')),
+                ),
+              ),
+            ),
+            SizedBox(height: height * .01),
+            Text(
+              "Covid-19 tracker",
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.w600),
+            ),
+          ],
         ),
       ),
     );
